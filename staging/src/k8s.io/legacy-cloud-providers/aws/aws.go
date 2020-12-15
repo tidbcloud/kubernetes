fd322b23f72ca429af64177568ff7af458d65c98
郑佳金 <zhengjiajin@pingcap.com>
2020-12-15 14:42:12 +0800

    support aws web identity provider

--- staging/src/k8s.io/legacy-cloud-providers/aws/aws.go
+++ staging/src/k8s.io/legacy-cloud-providers/aws/aws.go
@@ -35,8 +35,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
-	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/endpoints"
 	"github.com/aws/aws-sdk-go/aws/request"
@@ -46,9 +44,8 @@ import (
 	"github.com/aws/aws-sdk-go/service/elb"
 	"github.com/aws/aws-sdk-go/service/elbv2"
 	"github.com/aws/aws-sdk-go/service/kms"
-	"github.com/aws/aws-sdk-go/service/sts"
 	"gopkg.in/gcfg.v1"
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/klog"
 
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -64,7 +61,7 @@ import (
 	"k8s.io/client-go/pkg/version"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/record"
-	"k8s.io/cloud-provider"
+	cloudprovider "k8s.io/cloud-provider"
 	nodehelpers "k8s.io/cloud-provider/node/helpers"
 	servicehelpers "k8s.io/cloud-provider/service/helpers"
 	cloudvolume "k8s.io/cloud-provider/volume"
@@ -1134,28 +1131,30 @@ func init() {
 		if err != nil {
 			return nil, fmt.Errorf("unable to initialize AWS session: %v", err)
 		}
-
-		var provider credentials.Provider
-		if cfg.Global.RoleARN == "" {
-			provider = &ec2rolecreds.EC2RoleProvider{
-				Client: ec2metadata.New(sess),
+		/*
+			var provider credentials.Provider
+			if cfg.Global.RoleARN == "" {
+				provider = &ec2rolecreds.EC2RoleProvider{
+					Client: ec2metadata.New(sess),
+				}
+			} else {
+				klog.Infof("Using AWS assumed role %v", cfg.Global.RoleARN)
+				provider = &stscreds.AssumeRoleProvider{
+					Client:  sts.New(sess),
+					RoleARN: cfg.Global.RoleARN,
+				}
 			}
-		} else {
-			klog.Infof("Using AWS assumed role %v", cfg.Global.RoleARN)
-			provider = &stscreds.AssumeRoleProvider{
-				Client:  sts.New(sess),
-				RoleARN: cfg.Global.RoleARN,
-			}
-		}
 
-		creds := credentials.NewChainCredentials(
-			[]credentials.Provider{
-				&credentials.EnvProvider{},
-				provider,
-				&credentials.SharedCredentialsProvider{},
-			})
+			creds := credentials.NewChainCredentials(
+				[]credentials.Provider{
+					&
+					&credentials.EnvProvider{},
+					provider,
+					&credentials.SharedCredentialsProvider{},
+				})
+		*/
 
-		aws := newAWSSDKProvider(creds, cfg)
+		aws := newAWSSDKProvider(_, cfg)
 		return newAWSCloud(*cfg, aws)
 	})
 }
